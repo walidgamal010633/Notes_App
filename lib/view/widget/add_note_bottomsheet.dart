4bd171ed8420ed6da52621c11efdac62df622a9e
walidgamal <walidgamal010633@gmail.com>
2023-12-21 00:24:38 +0200

    provider add note cubit

--- lib/view/widget/add_note_bottomsheet.dart
+++ lib/view/widget/add_note_bottomsheet.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/cubits/cubit/add_note_cubit.dart';
 import 'package:notes_app/view/widget/add_note_form.dart';
-
 
 class addnotebottomsheet extends StatelessWidget {
   const addnotebottomsheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16,vertical: 24),
-        child: add_note_form(),
+    return BlocProvider(
+      create: (context) => AddNoteCubit(),
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 24),
+          child: add_note_form(),
+        ),
       ),
     );
   }
