ef6e1996c9ea80ad9a4f870ff9ec473e647a2c46
walidgamal <walidgamal010633@gmail.com>
2023-12-20 21:13:15 +0200

    add note state

--- lib/cubits/cubit/add_note_cubit.dart
+++ lib/cubits/cubit/add_note_cubit.dart
@@ -1,4 +1,5 @@
 import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
 
 part 'add_note_state.dart';
--- lib/cubits/cubit/add_note_state.dart
+++ lib/cubits/cubit/add_note_state.dart
@@ -4,4 +4,12 @@ part of 'add_note_cubit.dart';
 sealed class AddNoteState {}
 
 final class AddNoteInitial extends AddNoteState {}
+final class AddNoteLoading extends AddNoteState {}
+final class AddNoteSuccess extends AddNoteState{}
+final class AddNoteFalir extends AddNoteState{
+
+  final String ErorrMassage ;
+
+  AddNoteFalir(this.ErorrMassage);
+}
 
